2921e031d72bcaa6ba260fb4dc56576c34fd8fd0
wrongwrong <boranti1995@gmail.com>
2021-01-23 23:53:24 +0900

    ベンチマーク設定を整理

--- build.gradle.kts
+++ build.gradle.kts
@@ -67,11 +67,15 @@ tasks {
 }
 
 jmh {
-    fork = 1
-    batchSize = 3
-    iterations = 3
+    warmupForks = 2
     warmupBatchSize = 3
     warmupIterations = 3
+    warmup = "1s"
+
+    fork = 2
+    batchSize = 3
+    iterations = 2
+    timeOnIteration = "1500ms"
 
     failOnError = true
     isIncludeTests = false

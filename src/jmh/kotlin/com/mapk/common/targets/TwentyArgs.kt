a00bc491ce3d1910e8dc57e89dc3f4ab4e659d2d
wrongwrong <boranti1995@gmail.com>
2021-01-23 18:09:23 +0900

    修正

--- src/jmh/kotlin/com/mapk/common/targets/TwentyArgs.kt
+++ src/jmh/kotlin/com/mapk/common/targets/TwentyArgs.kt
@@ -1,24 +1,27 @@
 package com.mapk.common.targets
 
+import com.mapk.common.NoArg
+
+@NoArg
 data class TwentyArgs(
-    val arg00: Int,
-    val arg01: Int,
-    val arg02: Int,
-    val arg03: Int,
-    val arg04: Int,
-    val arg05: Int,
-    val arg06: Int,
-    val arg07: Int,
-    val arg08: Int,
-    val arg09: Int,
-    val arg10: Int,
-    val arg11: Int,
-    val arg12: Int,
-    val arg13: Int,
-    val arg14: Int,
-    val arg15: Int,
-    val arg16: Int,
-    val arg17: Int,
-    val arg18: Int,
-    val arg19: Int
+    var arg00: Int,
+    var arg01: Int,
+    var arg02: Int,
+    var arg03: Int,
+    var arg04: Int,
+    var arg05: Int,
+    var arg06: Int,
+    var arg07: Int,
+    var arg08: Int,
+    var arg09: Int,
+    var arg10: Int,
+    var arg11: Int,
+    var arg12: Int,
+    var arg13: Int,
+    var arg14: Int,
+    var arg15: Int,
+    var arg16: Int,
+    var arg17: Int,
+    var arg18: Int,
+    var arg19: Int
 )

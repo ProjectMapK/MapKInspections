5ab53c3b925e0a00281917ac9560d317913e2c4e
wrongwrong <boranti1995@gmail.com>
2021-01-31 00:06:45 +0900

    コンストラクタ呼び出しのためのdstを追加

--- src/jmh/kotlin/com/mapk/common/targets/FiveArgs.kt
+++ src/jmh/kotlin/com/mapk/common/targets/FiveArgs.kt
@@ -10,3 +10,12 @@ data class FiveArgs(
     var arg3: Int,
     var arg4: Int
 )
+
+// MapStructは無引数コンストラクタが有るとコンストラクタ呼び出しが機能しない
+data class FiveArgs2(
+    var arg0: Int,
+    var arg1: Int,
+    var arg2: Int,
+    var arg3: Int,
+    var arg4: Int
+)

5fc0a3891caa418670dd929f121666e52e9f3973
wrongwrong <boranti1995@gmail.com>
2021-01-30 12:35:49 +0900

    ソースクラスを追加

---
+++ src/jmh/kotlin/com/mapk/common/sources/FiveArgsSrc.kt
@@ -0,0 +1,9 @@
+package com.mapk.common.sources
+
+data class FiveArgsSrc(
+    var arg0: Int,
+    var arg1: Int,
+    var arg2: Int,
+    var arg3: Int,
+    var arg4: Int
+)
---
+++ src/jmh/kotlin/com/mapk/common/sources/TwentyArgsSrc.kt
@@ -0,0 +1,31 @@
+package com.mapk.common.sources
+
+data class TwentyArgsSrc(
+    var arg00: Int,
+    var arg01: Int,
+    var arg02: Int,
+    var arg03: Int,
+    var arg04: Int,
+    var arg05: Int,
+    var arg06: Int,
+    var arg07: Int,
+    var arg08: Int,
+    var arg09: Int,
+    var arg10: Int,
+    var arg11: Int,
+    var arg12: Int,
+    var arg13: Int,
+    var arg14: Int,
+    var arg15: Int,
+    var arg16: Int,
+    var arg17: Int,
+    var arg18: Int,
+    var arg19: Int
+) {
+    companion object {
+        fun of(arg: Int) = TwentyArgsSrc(
+            arg, arg, arg, arg, arg, arg, arg, arg, arg, arg,
+            arg, arg, arg, arg, arg, arg, arg, arg, arg, arg
+        )
+    }
+}

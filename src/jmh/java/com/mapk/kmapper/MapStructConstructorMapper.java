de350bb122fb9ff6e968067fbb87a737a3a91a26
wrongwrong <boranti1995@gmail.com>
2021-01-31 03:00:13 +0900

    TwentyArgs向けマッパーを追加

--- src/jmh/java/com/mapk/kmapper/MapStructConstructorMapper.java
+++ src/jmh/java/com/mapk/kmapper/MapStructConstructorMapper.java
@@ -1,11 +1,15 @@
 package com.mapk.kmapper;
 
 import com.mapk.common.sources.FiveArgsSrc;
+import com.mapk.common.sources.TwentyArgsSrc;
 import com.mapk.common.targets.FiveArgs2;
+import com.mapk.common.targets.TwentyArgs2;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 
 @Mapper(injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface MapStructConstructorMapper {
     FiveArgs2 map(FiveArgsSrc src);
+
+    TwentyArgs2 map(TwentyArgsSrc src);
 }
--- src/jmh/java/com/mapk/kmapper/MapStructMapper.java
+++ src/jmh/java/com/mapk/kmapper/MapStructMapper.java
@@ -1,10 +1,14 @@
 package com.mapk.kmapper;
 
 import com.mapk.common.sources.FiveArgsSrc;
+import com.mapk.common.sources.TwentyArgsSrc;
 import com.mapk.common.targets.FiveArgs;
+import com.mapk.common.targets.TwentyArgs;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface MapStructMapper {
     FiveArgs map(FiveArgsSrc src);
+
+    TwentyArgs map(TwentyArgsSrc src);
 }
